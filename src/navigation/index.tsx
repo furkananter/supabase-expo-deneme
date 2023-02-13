e043c10e0a305814ec2a17d59a8b084dee116de2
furky.eth <anterfurkan@gmail.com>
2023-02-13 21:56:04 +0300

    App.tsx

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,7 +1,5 @@
 import { View, Text } from 'react-native';
 import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import { SafeAreaView } from 'react-native-safe-area-context';
 
